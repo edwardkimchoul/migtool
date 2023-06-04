10e40834c59796bd9414e3cccb1137302abce9cd
edwardkimchoul <tncpro2003@gmail.com>
2023-06-04 18:38:35 +0900

    SQL 수정

--- migtool/src/main/resources/mig_work_list.sql
+++ migtool/src/main/resources/mig_work_list.sql
@@ -1,5 +1,4 @@
 DROP TABLE UGENS.MIG_PROCESS_LIST CASCADE CONSTRAINTS;
-
 CREATE TABLE UGENS.MIG_PROCESS_LIST (     
 				PROCESS_ID              NUMBER(10)    PRIMARY KEY  
 			,   BUSINESS_UNIT           VARCHAR2(30)            /* 진료, 간호 */
@@ -13,12 +12,10 @@ CREATE TABLE UGENS.MIG_PROCESS_LIST (
 			,	UPD_DATE                DATE   DEFAULT SYSDATE
 );
 
+
 DROP TABLE UGENS.MIG_PROCESS_HIST CASCADE CONSTRAINTS;
-<<<<<<< HEAD
-CREATE TABLE UGENS.MIG_PROCESS_HIST {
-=======
 CREATE TABLE UGENS.MIG_PROCESS_HIST (
->>>>>>> branch 'master' of https://github.com/edwardkimchoul/migtool
+
         MIG_HIST_NAME          VARCHAR2(20)   /* 이행한 날짜를 넣을것 2022-05-23  이행수행단위로 관리하기 위함  */    
 			,	PROCESS_ID             NUMBER(10)
 			,	START_TIME             VARCHAR2(30)           /* 시작일시 */
@@ -28,8 +25,32 @@ CREATE TABLE UGENS.MIG_PROCESS_HIST (
 			,	ERROR                  VARCHAR2(4000)         /* 에러가 있으면 넣음 */
 			, CRE_DATE               DATE   DEFAULT SYSDATE 
 			, constraint MIG_PROCESS_HIST_PK1 PRIMARY KEY ( MIG_HIST_NAME   , PROCESS_ID ) 
-<<<<<<< HEAD
-)
-=======
 );
->>>>>>> branch 'master' of https://github.com/edwardkimchoul/migtool
+
+
+CREATE OR REPLACE FUNCTION FN_MIG_NOT_COMPLETE_CNT (MIG_WORK_LIST VARCHAR2) 
+    RETURN NUMBER 
+IS 
+    row_cnt NUMBER;    /* 미완료 갯수 */ 
+BEGIN 
+	
+    SELECT count(1) CNT 
+      INTO row_cnt 
+      FROM MIG_PROCESS_LIST 
+     WHERE PROCESS_ID IN ( SELECT REGEXP_SUBSTR(a.mig_work_list, '[^,]+', 1, LEVEL) AS process_id
+                             FROM (SELECT MIG_WORK_LIST AS mig_work_list 
+                                     FROM dual) a
+                           CONNECT BY LEVEL <= LENGTH(REGEXP_REPLACE(a.mig_work_list, '[^|]+','')) + 1 )
+      AND  STATUS != 'C'                     
+
+    RETURN row_cnt; 
+END;    
+
+
+SELECT M.PROCESS_ID, M.DB_NAME, M.PROCEDURE_NAME, M.SQL_TYPE_CD
+  FROM MIG_PROCESS_LIST M
+ WHERE M.MIG_STATUS_CD = 'R'   
+   AND FN_MIG_NOT_COMPLETE_CNT( M.PRECEDING_WORK_LIST ) = 0
+   ORDER BY PROCESS_ID;
+   
+
